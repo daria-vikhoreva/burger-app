b0eb0761b8e88706390522aff4e6075b85d96472
Daria Vikhoreva <darja.vihoreva@yandex.ru>
2023-05-04 21:22:06 +0400

    added import components in App and fonts

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,70 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="wrapper">
+        <CardItem/>
+        <UiButton/>
+        <UiInput/>
+        <UiCheckbox/>
+        <UiMultipleSelect/>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CardItem from './components/CardItem.vue';
+import UiButton from './components/UI/UiButton.vue';
+import UiInput from './components/UI/UiInput.vue';
+import UiCheckbox from './components/UI/UiCheckbox.vue';
+import UiMultipleSelect from './components/UI/UiMultipleSelect.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CardItem,
+    UiButton,
+    UiInput,
+    UiCheckbox,
+    UiMultipleSelect,
   }
 }
 </script>
 
 <style>
+
+@font-face {
+  font-family: "Avenir";
+  src: local("Avenir"),  
+  url(./assets/fonts/AvenirLTStd-Black.otf) format("opentype");
+  font-weight: 700;
+}
+
+@font-face {
+  font-family: "Avenir";
+  src: local("Avenir"),  
+  url(./assets/fonts/AvenirLTStd-Book.otf) format("opentype");
+  font-weight: 350;
+}
+
+@font-face {
+  font-family: "Avenir";
+  src: local("Avenir"),  
+  url(./assets/fonts/AvenirLTStd-Roman.otf) format("opentype");
+  font-weight: 400;
+}
+
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  font-family: "Avenir", sans-serif;
   margin-top: 60px;
+  font-size: 12px;
+  font-weight: 400;
+  margin-left: 60px;
+}
+
+* {
+  box-sizing: border-box;
 }
 </style>
