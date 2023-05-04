59f67f50b719e8902089630c9e4bc0c2578db8c7
Daria Vikhoreva <darja.vihoreva@yandex.ru>
2023-05-04 21:16:55 +0400

    added HTML, CSS for UiButton

---
+++ src/components/UI/UiButton.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <button class="button">Заказать</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'UiButton'
+    }
+</script>
+
+<style scoped>
+.button {
+    background: #FF0000;
+    border-radius: 7px;
+    padding: 9px 17px;
+    color: #FFFFFF;
+    font-weight: 500;
+    font-size: 18px;
+    border: none;
+    cursor: pointer;
+}
+</style>
