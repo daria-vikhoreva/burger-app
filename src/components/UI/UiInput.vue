d6f46cedc88ed23551ec79a10823ff69d9cdaf9f
Daria Vikhoreva <darja.vihoreva@yandex.ru>
2023-05-04 21:19:07 +0400

    added HTML, CSS for UiInput

---
+++ src/components/UI/UiInput.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <input type="text" class="input" placeholder="Search">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'UiInput'
+    }
+</script>
+
+<style scoped>
+.input {
+    background: linear-gradient(98.81deg, #FFF0F0 -0.82%, #FFDFDF 101.53%);
+    border-radius: 7px;
+    padding: 10px 18px;
+    border: none;
+}
+.input::placeholder {
+    font-size: 12px;
+    color: #000;
+    opacity: 0.9;
+}
+</style>
