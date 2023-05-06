56d954dc52342f6de53f2ccdc1d09e54f8c16a06
Daria Vikhoreva <darja.vihoreva@yandex.ru>
2023-05-06 18:43:03 +0400

    added HTML, CSS for CardList

---
+++ src/components/CardList.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <div class="card-list">
+            <CardItem/>
+            <CardItem/>
+            <CardItem/>
+            <CardItem/>
+            <CardItem/>
+            <CardItem/>
+        </div>
+    </div>
+</template>
+
+<script>
+import CardItem from './CardItem.vue';
+
+    export default {
+        name: 'CardList',
+        components: {
+            CardItem,
+        }
+    }
+</script>
+
+<style scoped>
+.card-list {
+    display: flex;
+    gap: 17px;
+    flex-wrap: wrap;
+}
+</style>
