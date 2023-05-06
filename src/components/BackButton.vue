4eb93685dd76582a725fe9f1a372a657dfd654aa
Daria Vikhoreva <darja.vihoreva@yandex.ru>
2023-05-06 19:59:25 +0400

    added HTML, CSS for BackButton

---
+++ src/components/BackButton.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="back-button">
+        <img src="../assets/images/path.svg" alt="back-path">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'BackButton'
+    }
+</script>
+
+<style scoped>
+.back-button {
+    background-color: rgba(255, 0, 0, 0.2);
+    border-radius: 7px;
+    padding: 14px 18px;
+    max-width: max-content;
+    display: flex;
+    cursor: pointer;
+}
+</style>
