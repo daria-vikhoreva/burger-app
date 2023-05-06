1f253f3c8a5b38758fedf5b3035e79c7bb3840c2
Daria Vikhoreva <darja.vihoreva@yandex.ru>
2023-05-06 18:36:38 +0400

    added HTML, CSS for UiLoader

---
+++ src/components/UI/UiLoader.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <img src="../../assets/images/loader.svg" class="loader" alt="loader">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'UiLoader',
+    }
+</script>
+
+<style scoped>
+@keyframes loading {
+    from {
+        transform:rotate(0deg);
+    }
+    to {
+        transform:rotate(360deg);
+    }
+}
+
+.loader {
+    animation: 1s linear infinite loading;
+}
+</style>
